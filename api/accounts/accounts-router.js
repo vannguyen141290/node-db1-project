cd8383b7bcac6457a2c073dfb13ced649b0447d6
thi <vannguyen141290@gmail.com>
2021-10-04 19:45:58 -0700

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,15 +1,27 @@
 const router = require('express').Router()
+const { checkAccountPayload, checkAccountNameUnique, checkAccountId } = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll()
+    res.status(200).json(accounts)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, (req, res) => {
+  res.status(200).json(req.found)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, async (req, res, next) => {
+  try {
+    const newAcc = await Account.create(req.body)
+    console.log(newAcc)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
@@ -21,7 +33,13 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
 })
+
+
+
 
 module.exports = router;
